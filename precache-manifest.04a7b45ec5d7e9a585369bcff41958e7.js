463799014fc4e5499c1d76ec371f466822d3325b
Oleg Loutchansky <luchansodev@gmail.com>
2020-04-05 18:46:28 +0300

    Updates

---
+++ precache-manifest.04a7b45ec5d7e9a585369bcff41958e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84fa6f3fedd4b63b7b7d2c3082583992",
+    "url": "/fair-salary-web/index.html"
+  },
+  {
+    "revision": "edfdb0e06db8c9846d10",
+    "url": "/fair-salary-web/static/js/2.9fbcd1db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fair-salary-web/static/js/2.9fbcd1db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b849ca95b1541ba1f7b1",
+    "url": "/fair-salary-web/static/js/main.f75f25b7.chunk.js"
+  },
+  {
+    "revision": "d72f5342eddd2174a0c8",
+    "url": "/fair-salary-web/static/js/runtime-main.ad0f7559.js"
+  }
+]);
