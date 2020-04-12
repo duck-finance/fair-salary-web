81ed47cbe2569d5606dd4f6273808a1286b4e75f
Oleg Loutchansky <luchansodev@gmail.com>
2020-04-12 15:48:30 +0300

    feat(ui): update app title

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, Fragment } from "react";
 import { Form, FormState } from "./components/Form/Form";
 import { Chart } from "./components/Chart/Chart";
+import { Typography } from "@material-ui/core";
 
 function App() {
   const [formState, handleChange] = useState<FormState>({
@@ -10,6 +11,9 @@ function App() {
 
   return (
     <Fragment>
+      <Typography variant="h1" align="center">
+        Справедливая зарплата
+      </Typography>
       <Form formState={formState} onChange={handleChange} />
       <Chart formState={formState} />
     </Fragment>
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -14,9 +14,6 @@ type Props = {
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-`;
-
-const FormContainer = styled.div`
   width: 240px;
 `;
 
@@ -35,22 +32,19 @@ export function Form({ formState, onChange }: Props) {
 
   return (
     <Container>
-      <h1>Справедливая зарплата</h1>
-      <FormContainer>
-        <p>Я получаю</p>
-        <input
-          placeholder="20 000 руб."
-          onChange={handleChangeSalary}
-          value={formState.salary}
-        />
-        <p>Последний раз менялась</p>
-        <input
-          type="date"
-          placeholder="Когда последний раз повышалась?"
-          value={formState.date}
-          onChange={handleChangeDate}
-        />
-      </FormContainer>
+      <p>Я получаю</p>
+      <input
+        placeholder="20 000 руб."
+        onChange={handleChangeSalary}
+        value={formState.salary}
+      />
+      <p>Последний раз менялась</p>
+      <input
+        type="date"
+        placeholder="Когда последний раз повышалась?"
+        value={formState.date}
+        onChange={handleChangeDate}
+      />
     </Container>
   );
 }
